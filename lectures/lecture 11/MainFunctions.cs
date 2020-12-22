b4995e45963aa65e885a0749ee528dc027fb7282
Furkan Gözükara <furkangozukara@gmail.com>
2020-12-23 01:17:20 +0300

    lecture 11 source code

---
+++ lectures/lecture 11/MainFunctions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace lecture_10
+{
+    public static class MainFunctions
+    {
+
+        public static Tuple<bool, string> tryToRegister(MainWindow mainWindowInstance)
+        {
+            var vrResult = helper_methods.checkIfUserNameValid(mainWindowInstance.txtUserName_Register.Text, mainWindowInstance.txtEmail.Text);
+
+            if (vrResult.blResult == false)
+            {
+                return new Tuple<bool, string>(vrResult.blResult, vrResult.srResultMessage);
+            }
+
+            vrResult = helper_methods.checkIfPasswordIsValid(mainWindowInstance.txtPassword1_register.Password.ToString(),//converting password box to text .Password
+                mainWindowInstance.txtPassword2_register.Password.ToString());
+
+            if (vrResult.blResult == false)
+            {
+                return new Tuple<bool, string>(vrResult.blResult, vrResult.srResultMessage);
+            }
+
+            if (!mainWindowInstance.txtEmail.Text.IsValidEmail())
+            {
+                vrResult.blResult = false;
+                vrResult.srResultMessage = "Your entered email is not a valid email address. Please check your email!";
+            }
+
+            if (vrResult.blResult == true)
+            {
+                File.AppendAllText(helper_methods.srUsersFileName, generateUserInfoFormat(mainWindowInstance.txtUserName_Register.Text,
+                    mainWindowInstance.txtPassword1_register.Password.ToString(),
+                    mainWindowInstance.txtEmail.Text));
+            }
+
+
+            return new Tuple<bool, string>(vrResult.blResult, vrResult.srResultMessage);
+        }
+
+        private static string generateUserInfoFormat(string srUsername, string srPassword, string srEmail)
+        {
+            return srUsername + helper_methods.crUserInfoSeperatorCharacter +
+                srPassword.ComputeSha256Hash() + helper_methods.crUserInfoSeperatorCharacter +
+                srEmail + Environment.NewLine;//you can use \r\n for new line as well
+        }
+
+
+    }
+}
---
+++ lectures/lecture 11/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lecture_10
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCompleteRegister_Click(object sender, RoutedEventArgs e)
+        {
+            Tuple<bool, string> registerResult = MainFunctions.tryToRegister(this);
+
+            if (registerResult.Item1 == false)
+                MessageBox.Show(registerResult.Item2);
+
+            if (registerResult.Item1)
+                MessageBox.Show("You have successfully registered. Now you will be automatically logged-in");           
+        }
+    }
+}
